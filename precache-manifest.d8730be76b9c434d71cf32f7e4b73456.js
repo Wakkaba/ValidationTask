e839148689f2f10cbeaf82aa4d28b5077224812f
Arthur Zarytsky <skyzarfranko@gmail.com>
2020-07-25 17:25:39 +0300

    Updates

---
+++ precache-manifest.d8730be76b9c434d71cf32f7e4b73456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ddbaa1f3e5e072b11d31b97d8f15d4",
+    "url": "/ValidationTask/index.html"
+  },
+  {
+    "revision": "a4bd829f836ebf5ad6b1",
+    "url": "/ValidationTask/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8251301240e73ad5b9f5",
+    "url": "/ValidationTask/static/js/2.6a256c94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ValidationTask/static/js/2.6a256c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4bd829f836ebf5ad6b1",
+    "url": "/ValidationTask/static/js/main.0dfecebc.chunk.js"
+  },
+  {
+    "revision": "c064e41d5a324d6f691c",
+    "url": "/ValidationTask/static/js/runtime-main.ccd1f3c7.js"
+  }
+]);
